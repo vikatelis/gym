58e658762d6a225421bc7d36bd7a7e6da29fa950
vikatelis <vikatelis@gmail.com>
2019-04-02 14:16:36 +0200

    test environments

---
+++ gym/envs/test_env.py
@@ -0,0 +1,44 @@
+#import logistic_regression
+import tensorflow as tf
+# import the inspect_checkpoint library
+from tensorflow.python.tools import inspect_checkpoint as chkp
+import numpy as np
+import pickle
+from baselines.common.atari_wrappers import make_atari, wrap_deepmind, make_rosi
+import gym
+from gym.utils import branin
+
+# init training env
+print("init env")
+#env_type, env_id = get_env_type("LogReg-v0")
+env_type, env_id = get_env_type("SGD-v0")
+env = gym.make(env_id)
+print(env)
+env.num_envs = 1
+#ßenv = make_rosenbrock_env(env_id, 1, None)
+obs = env.reset()
+ob_space = env.observation_space
+print(ob_space.shape)
+ac_space = env.action_space
+print(ac_space.shape)
+
+
+step = 0
+done = False
+obs = env.reset()
+while done == False and step<=20:
+    actions = 0.1
+    obs, _, done, info  = env.step(actions)
+    print("loss: ", str(info['loss']))
+    #print("config: ", str(info['config']))
+    done = done.any() if isinstance(done, np.ndarray) else done
+
+    if done:
+        print(done)
+        obs = env.reset()
+'''
+# run it
+out = logReg.objective_function([-3,1,20,0.0], fold=10)
+print(type(out))
+print(out['function_value'])
+'''
