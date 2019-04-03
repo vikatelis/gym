dec8a0a5b4763401ed108ea6bcff30b328e9748c
vikatelis <vikatelis@gmail.com>
2019-04-03 16:45:38 +0200

    add standard cnn function

---
+++ gym/utils/cnn_standard.py
@@ -0,0 +1,36 @@
+import tensorflow as tf
+
+def cnn_model_fn(input, mode):
+  """Model function for CNN."""
+  print("input shape ", str(input.shape))
+
+  # Convolutional Layer #1
+  conv1 = tf.layers.conv2d(
+      inputs=input,
+      filters=32,
+      kernel_size=[5, 5],
+      padding="same",
+      activation=tf.nn.relu)
+
+  # Pooling Layer #1
+  pool1 = tf.layers.max_pooling2d(inputs=conv1, pool_size=[2, 2], strides=2)
+
+  # Convolutional Layer #2 and Pooling Layer #2
+  conv2 = tf.layers.conv2d(
+      inputs=pool1,
+      filters=64,
+      kernel_size=[5, 5],
+      padding="same",
+      activation=tf.nn.relu)
+  pool2 = tf.layers.max_pooling2d(inputs=conv2, pool_size=[2, 2], strides=2)
+
+  # Dense Layer
+  pool2_flat = tf.reshape(pool2, [-1, 7 * 7 * 64 * 3])
+  dense = tf.layers.dense(inputs=pool2_flat, units=1024, activation=tf.nn.relu)
+  dropout = tf.layers.dropout(
+      inputs=dense, rate=0.4, training=mode == tf.estimator.ModeKeys.TRAIN)
+
+  # Logits Layer
+  logits = tf.layers.dense(inputs=dropout, units=10)
+
+  return logits
